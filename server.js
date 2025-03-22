8723f3c6b84a03d4fc8889b71bc267343ed383ce
etijoroemmanuel <etijoroemmanuel1@gmail.com>
2025-03-22 15:15:17 +0100

    morgan

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 require('./config/database');
 const weatherRouter = require('./routes/weatherRoutes')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const PORT = 1127;
 
 const app = express();
 app.use(cors({origin: "*"}));
-app.use(express.json())
-app.use(weatherRouter)
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(weatherRouter);
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening to PORT:${PORT}`)
